a36511173c788061f925d97a50eac16cef20c9eb
nislambsl <nazrulbsl7@gmail.com>
2021-09-08 17:15:05 +0600

    Promo code updated

--- js/index.js
+++ js/index.js
@@ -15,7 +15,12 @@ const deleveryCost = document.getElementById('delivery-cost');
 
 const bestPrice = document.getElementById('best-price');
 const totalPrice = document.getElementById('total-price');
+const total = document.getElementById('total');
+const applyBtn = document.getElementById('apply');
+const promoCode = document.getElementById('promo-code');
 
+
+// Calculate Sub-Total
 function upateTotalPrice(){
     const productPrice = Number(bestPrice.innerText);
     const extraMemoryCharge = Number(extraMemory.innerText);
@@ -24,40 +29,59 @@ function upateTotalPrice(){
     const subTotal = productPrice + extraMemoryCharge + extraStorageCharge + deleveryCharge;
     totalPrice.innerText =subTotal;
     
-//console.log(extraMemory.innerText, bestPrice.innerText, extraStorage.innerText, deleveryCost.innerText)
 };
+
+//Calculate Total Pay
+function updateTotalPay (){
+    const basePrice = Number(bestPrice.innerText);
+    const memoryCharge = Number(extraMemory.innerText);
+    const storageCharge = Number(extraStorage.innerText);
+    const delevery = Number(deleveryCost.innerText);
+    const totalPay = basePrice + memoryCharge + storageCharge + delevery;
+    total.innerText = totalPay;
+}
 
 minMemory.addEventListener('click', function(){
     extraMemory.innerText = '00';
     bestPrice.innerText = '1299';
     upateTotalPrice()
-       
+    updateTotalPay ()
+           
    });
 maxMemory.addEventListener('click', function(){
     extraMemory.innerText = '180';
     bestPrice.innerText = '1299'; 
     upateTotalPrice() 
+    updateTotalPay ()   
  
    });
 minStorage.addEventListener('click', function(){
     extraStorage.innerText = '00';
     bestPrice.innerText = '1299';  
     upateTotalPrice()
+    updateTotalPay ()
+    
     });  
 xStorage.addEventListener('click', function(){
     extraStorage.innerText = '100';
     bestPrice.innerText = '1299';  
     upateTotalPrice()
+    updateTotalPay ()
+    
         });  
 xlStorage.addEventListener('click', function(){
     extraStorage.innerText = '180';
     bestPrice.innerText = '1299';  
     upateTotalPrice()
+    updateTotalPay ()
+   
         });  
 freeDelevery.addEventListener('click', function(){
     deleveryCost.innerText = '00',
     bestPrice.innerText = '1299'; 
     upateTotalPrice()
+    updateTotalPay ()
+   
 });
   
 
@@ -65,5 +89,14 @@ expreesDelevery.addEventListener('click', function(){
     deleveryCost.innerText = '20',
     bestPrice.innerText = '1299'; 
     upateTotalPrice()
+    updateTotalPay ()
+    
 });
-  
+// Apply Promo Code
+applyBtn.addEventListener('click', function(){
+
+   if (promoCode.value == 'stevekaku'){ total.innerText = totalPrice.innerText - totalPrice.innerText * .20;
+    promoCode.value = '';  
+
+   }
+});
